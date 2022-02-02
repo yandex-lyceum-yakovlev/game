f083006fea54f44d8f57776af9be68bb71422998
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2022-02-02 16:16:28 +0400

    update

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import tkinter
+from random import randint
+
+
+def draw(board):  # отображает внутреннее представление игрового поля на канвас
+    for i in range(n):
+        for j in range(m):
+            c.create_rectangle(i * cell_width, j * cell_height,
+                               (i + 1) * cell_width, (j + 1) * cell_height, fill=board[i][j])
+
+
+def update():
+    board[randint(0, n - 1)][randint(0, m - 1)] = "green"
+    draw(board)
+    window.after(1000, update)
+
 
 window = tkinter.Tk()
 
@@ -9,9 +24,11 @@ c.pack()
 n = m = 10  # количество строк и столбцов
 cell_width = width // n  # ширина ячейки
 cell_height = height // m  # высота ячейки
-for i in range(n):
-    for j in range(m):
-        c.create_rectangle(i * cell_width, j * cell_height,
-                           (i + 1) * cell_width, (j + 1) * cell_height, fill="red")
+
+board = [["red"] * n for i in range(m)]
+board[2][5] = "green"
+board[3][0] = "blue"
+draw(board)
+window.after(1000, update)
 
 window.mainloop()
