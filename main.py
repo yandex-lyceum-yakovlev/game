68141362fc06fccdc4b295d8e56178f83a8b7c98
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2022-02-02 16:03:54 +0400

    grid

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import tkinter
 
 window = tkinter.Tk()
 
-c = tkinter.Canvas(window, width=600, height=600, bg='white')
+width = height = 600  # ширина и высота игрового поля в пикселях
+c = tkinter.Canvas(window, width=width, height=height, bg='white')
 c.pack()
 
-c.create_rectangle(100, 100, 200, 200, fill="red")
+n = m = 10  # количество строк и столбцов
+cell_width = width // n  # ширина ячейки
+cell_height = height // m  # высота ячейки
+for i in range(n):
+    for j in range(m):
+        c.create_rectangle(i * cell_width, j * cell_height,
+                           (i + 1) * cell_width, (j + 1) * cell_height, fill="red")
 
 window.mainloop()
